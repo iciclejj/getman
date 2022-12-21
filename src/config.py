5b2cc28fd29f97908b7b354aec8bf2869fcbd165
icicle <55782191+iciclejj@users.noreply.github.com>
2022-12-21 16:18:15 +0100

    config.py lint, improve open()

--- src/config.py
+++ src/config.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 from constants import (
         FILE_PATH_CONFIG,
@@ -11,13 +10,13 @@ def init_config():
 
     config_json = json.dumps(config_dict)
 
-    with open(FILE_PATH_CONFIG, 'w+') as file:
-        file.write(config_json)
+    with open(FILE_PATH_CONFIG, 'w+', encoding='utf-8') as config_file:
+        config_file.write(config_json)
 
     return FILE_PATH_CONFIG
 
 def get_config():
-    with open(FILE_PATH_CONFIG) as config_file:
+    with open(FILE_PATH_CONFIG, 'r', encoding='utf-8') as config_file:
         config_dict = json.loads(config_file.read())
 
     return config_dict
